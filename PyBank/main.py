13e75dff42f2b64ba73bfb5c3b731135e6433501
Alideniz62 <alideniz62@gmail.com>
2020-09-09 22:16:12 -0700

    Python challenges updated

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,83 @@
+#______________________PyBank__________________________
 
+# In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. You will give a set of financial data called [budget_data.csv](PyBank/Resources/budget_data.csv). The dataset is composed of two columns: `Date` and `Profit/Losses`. (Thankfully, your company has rather lax standards for accounting so the records are simple.)
+
+# Your task is to create a Python script that analyzes the records to calculate each of the following:
+
+  # The total number of months included in the dataset
+
+  # The net total amount of "Profit/Losses" over the entire period
+
+  # The average of the changes in "Profit/Losses" over the entire period
+
+  # The greatest increase in profits (date and amount) over the entire period
+
+  # The greatest decrease in losses (date and amount) over the entire period
+
+
+
+#Importing the necessary modules/libraries
+import os
+import csv
+
+#Creating an object out of the CSV file
+csvpath = os.path.join("Resources", "budget_data.csv")
+#print(csvpath)
+
+# Open and read the CSV
+with open(csvpath, newline="") as csvfile:
+    #print(csvreader)
+    
+    # Read the header row and printing
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    
+ 
+    # Declare variables
+    Dates = []
+    Profit_Losses = []
+    Value = []
+    Increase_D = ""
+    Decrease_D = ""
+    
+    
+    for row in csvreader:
+        Dates.append(row[0])
+        Profit_Losses.append(int(row[1]))
+      
+    for i in range(1, len(Profit_Losses)):
+        
+        # Change between_months
+        Value.append(Profit_Losses[i] - Profit_Losses[i-1])
+        
+        # Finding average
+        Average_Change = sum(Value) / len(Value)
+        
+        # Greatest increase/decrease and date
+        Greatest_Increase = max(Value)
+        Increase_D = str(Dates[Value.index(max(Value))])
+        
+        Greatest_Decrease = min(Value)
+        Decrease_D = str(Dates[Value.index(min(Value))])
+        
+    
+    print("Financial Analysis")
+    print("-------------------------------")
+    print("Total Months: ", len(Dates))
+    print("Total: $", sum(Profit_Losses))
+    print("Average Change: $", round(Average_Change,2))
+    print("Greatest Increase: ", Increase_D, "($", Greatest_Increase,")")
+    print("Greatest Decrease: ", Decrease_D, "($", Greatest_Decrease,")")
+    
+    pybank = open("pybank.txt", "w")
+
+    text1 = "Financial Analysis"
+    text2 = "---------------------"
+    text3 = "Total Months: ", len(Dates)
+    text4 = "Total: $", sum(Profit_Losses)
+    text5 = "Average Change: $", round(Average_Change,2)
+    text6 = "Greatest Increase: ", Increase_D, "($", Greatest_Increase,")"
+    text7 = "Greatest Decrease: ", Decrease_D, "($", Greatest_Decrease,")"
+    pybank.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(text1,text2,text3,text4,text5,text6,text7))
+    
+    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,116 @@
 
+# ______________PyPoll challenge_________________
+
+# In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
+
+# A complete list of candidates who received votes
+
+# The percentage of votes each candidate won
+
+# The total number of votes each candidate won
+
+# The winner of the election based on popular vote.
+
+
+
+#import the os module and csv module
+import os
+import csv
+from decimal import Decimal
+pypoll_csv = os.path.join("Resources", "election_data.csv")
+
+with open(pypoll_csv) as csvfile:
+    importedCSV = csv.reader(csvfile, delimiter=',')
+    
+    voter_list = []
+    county_list = []
+    candidates = []
+    
+
+    for row in importedCSV:
+        voter_id = row[0]
+        county = row[1]
+        cand = row[2]
+
+#append the data
+        voter_list.append(voter_id)
+        county_list.append(county)
+        candidates.append(cand)
+
+#removing headers from list data.
+voterID = voter_list[1:len(voter_list)]
+county = county_list[1:len(county_list)]
+candidate = candidates[1:len(candidates)]
+
+#calulating total votes
+t_Votes = len(candidate)
+
+print("Election Results")
+print("------------------------------")
+print("Total Votes: {num}".format(num=t_Votes))
+
+
+#start working on the lists
+candidate_List=list(set(candidate))
+
+
+#keep tally for four candidates
+#each value for each candidate
+Votes0=candidate.count(candidate_List[0])
+Votes1=candidate.count(candidate_List[1])
+Votes2=candidate.count(candidate_List[2])
+Votes3=candidate.count(candidate_List[3])
+
+#creating vote percentages
+Per0 = Decimal(Votes0/t_Votes*100)
+Per1 = Decimal(Votes1/t_Votes*100)
+Per2 = Decimal(Votes2/t_Votes*100)
+Per3 = Decimal(Votes3/t_Votes*100)
+PerDes0 = round(Per0,3)
+PerDes1 = round(Per1,3)
+PerDes2 = round(Per2,3)
+PerDes3 = round(Per3,3)
+
+print("------------------------------")
+print("{one}: {two}% ({three})".format(one=candidate_List[0], two=PerDes0, three=Votes0))
+print("{one}: {two}% ({three})".format(one=candidate_List[1], two=PerDes1, three=Votes1))
+print("{one}: {two}% ({three})".format(one=candidate_List[2], two=PerDes2, three=Votes2))
+print("{one}: {two}% ({three})".format(one=candidate_List[3], two=PerDes3, three=Votes3))
+
+#determe winner
+wlist = []
+wlist.append(Votes0)
+wlist.append(Votes1)
+wlist.append(Votes2)
+wlist.append(Votes3)
+BW= wlist.index(int(max(wlist)))
+winner_cand= candidate_List[BW]
+
+print("------------------------------")
+print("Winner: {str}".format(str=winner_cand))
+print("------------------------------")
+
+text_file = open("pyboll.txt", "w")
+text_file.write("Election Results")
+text_file.write("\n")
+text_file.write("------------------------------")
+text_file.write("\n")
+text_file.write("Total Votes: {num}".format(num=t_Votes))
+text_file.write("\n")
+text_file.write("\n")
+text_file.write("------------------------------")
+text_file.write("\n")
+text_file.write("{one}: {two}% ({three})".format(one=candidate_List[0], two=PerDes0, three=Votes0))
+text_file.write("\n")
+text_file.write("{one}: {two}% ({three})".format(one=candidate_List[1], two=PerDes1, three=Votes1))
+text_file.write("\n")
+text_file.write("{one}: {two}% ({three})".format(one=candidate_List[2], two=PerDes2, three=Votes2))
+text_file.write("\n")
+text_file.write("{one}: {two}% ({three})".format(one=candidate_List[3], two=PerDes3, three=Votes3))
+text_file.write("\n")
+text_file.write("------------------------------")
+text_file.write("\n")
+text_file.write("Winner: {str}".format(str=winner_cand))
+text_file.write("\n")
+text_file.write("------------------------------")
+text_file.close()
